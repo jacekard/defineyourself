a4eacacd69ef79754740bbd12c34745aad6e1f90
jacekard <jacekard@gmail.com>
2018-08-29 23:30:43 +0200

    Testing home controller and layout for further use

---
+++ YouDefine/YouDefine/wwwroot/js/JavaScript.js
@@ -0,0 +1,31 @@
+﻿//(function () {
+
+//})();
+
+function loadBackgroundColor() {
+    $("body").animate({ backgroundColor: "red" }, "slow");
+    console.log("chuj kurwa");
+}
+
+//(function () {
+//    loadBackgroundColor();
+//    $("#search-input").click(function () {
+
+//    }
+
+
+//})();
+ 
+$("#search-input").click(function () {
+    $("body").addClass('yellow');
+});
+
+//$.ajax({
+//    url: "/api/getWeather",
+//    data: {
+//        zipcode: 97201
+//    },
+//    success: function (result) {
+//        $("#weather-temp").html("<strong>" + result + "</strong> degrees");
+//    }
+//});
