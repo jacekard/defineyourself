5aad480ab64b229cea658fe35362e3c68ac3ba0c
jacekard <jacekard@gmail.com>
2018-09-24 16:51:02 +0200

    Added reCAPTCHA script (not configured yet) and fixed styles

--- YouDefine/YouDefine/wwwroot/js/reportbugs.js
+++ YouDefine/YouDefine/wwwroot/js/reportbugs.js
@@ -1,6 +1,13 @@
 ﻿(function () {
     console.log("reportbugs dzialam");
 
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6Ldd3HEUAAAAABwaxh3XsxBxRBrKsmuGIltNKr7S', { action: 'reporting' })
+            .then(function (token) {
+                console.log(token);
+            });
+    });
+
     $("#bug-submit").click(function () {
         var response = $("#bug-report-text").val();
         if (response === "") {
