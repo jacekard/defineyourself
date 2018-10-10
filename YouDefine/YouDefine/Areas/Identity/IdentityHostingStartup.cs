249bca3c0726ebb541686a0923ccc4ca3cc6ed9d
jacekard <jacekard@gmail.com>
2018-10-10 23:08:42 +0200

    Added new scaffolding 'Identity'

---
+++ YouDefine/YouDefine/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YouDefine.Data;
+
+[assembly: HostingStartup(typeof(YouDefine.Areas.Identity.IdentityHostingStartup))]
+namespace YouDefine.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
